b39abe09f6555f63066f3715238a734ea6604bb7
Fengxin Dai <fengx@bu.edu>
2023-12-01 11:57:04 -0500

    fix(P1): fix copilot bug

--- P1.cpp
+++ P1.cpp
@@ -9,28 +9,24 @@ int main(){
     while(true){
         std::cout << "Enter command: ";
         std::cin >> command;
-        if(command == "add"){
+        if(command == "a"){
             std::cin >> bookName;
-            if(library.addBook(bookName)){
-                std::cout << "Book added" << std::endl;
-            }else{
-                std::cout << "Book already exists or library is full" << std::endl;
+            if(!library.addBook(bookName)){
+                std::cout << "Cannot add the book." << std::endl;
             }
-        }else if(command == "remove"){
+        }else if(command == "r"){
             std::cin >> bookName;
-            if(library.removeBook(bookName)){
-                std::cout << "Book removed" << std::endl;
-            }else{
-                std::cout << "Book does not exist" << std::endl;
+            if(!library.removeBook(bookName)){
+                std::cout << "Cannot remove the book." << std::endl;
             }
-        }else if(command == "print"){
+        }else if(command == "p"){
             library.print();
-        }else if(command == "quit"){
+        }else if(command == "q"){
+            std::cout << "Exiting" << std::endl;
             break;
         }else{
             std::cout << "Invalid command" << std::endl;
         }
     }
-
-
+    return 0;
 }
